7b4742b0690d779225f8c4473d61e8a6373028e1
Vidar <vidar@fugroup.net>
2018-07-09 02:17:18 +0200

    Renamed some variables

--- src/modules/brage.js
+++ src/modules/brage.js
@@ -1,10 +1,8 @@
 // Parse arguments
-const parse = (c) => {
-  const o = {
-    children: []
-  }
+const parse = (a) => {
+  const o = { children: [] }
 
-  for (const x of c) {
+  for (const x of a) {
     switch (x.constructor) {
       case String:
         o.str = x; break
@@ -19,8 +17,8 @@ const parse = (c) => {
 }
 
 // Create tags, apply attributes and children
-export const t = (tag, ...c) => {
-  let { str = '', atts = {}, children } = parse(c)
+export const t = (tag, ...a) => {
+  let { str = '', atts = {}, children } = parse(a)
 
   let el
   if (tag === 'fragment') {
@@ -29,13 +27,13 @@ export const t = (tag, ...c) => {
     el = document.createElement(tag)
     el.textContent = str
 
-    for (const a in atts) {
-      el.setAttribute(a, atts[a])
+    for (const x in atts) {
+      el.setAttribute(x, atts[x])
     }
   }
 
-  for (const child of children) {
-    el.appendChild(child)
+  for (const c of children) {
+    el.appendChild(c)
   }
 
   return el
@@ -52,121 +50,121 @@ export const mount = (children, parent) => {
   }
 
   parent.innerHTML = ''
-  for (const child of children) {
-    parent.appendChild(child)
+  for (const c of children) {
+    parent.appendChild(c)
   }
 }
 
 // Fragment, element without tags
-export const fragment = (...c) => { return t('fragment', ...c)}
+export const fragment = (...a) => { return t('fragment', ...a)}
 
 // Tags, all HTML5 tags are available
-export const a = (...c) => { return t('a', ...c)}
-export const abbr = (...c) => { return t('abbr', ...c)}
-export const address = (...c) => { return t('address', ...c)}
-export const area = (...c) => { return t('area', ...c)}
-export const article = (...c) => { return t('article', ...c)}
-export const aside = (...c) => { return t('aside', ...c)}
-export const audio = (...c) => { return t('audio', ...c)}
-export const b = (...c) => { return t('b', ...c)}
-export const base = (...c) => { return t('base', ...c)}
-export const bdi = (...c) => { return t('bdi', ...c)}
-export const bdo = (...c) => { return t('bdo', ...c)}
-export const blockquote = (...c) => { return t('blockquote', ...c)}
-export const body = (...c) => { return t('body', ...c)}
-export const br = (...c) => { return t('br', ...c)}
-export const button = (...c) => { return t('button', ...c)}
-export const canvas = (...c) => { return t('canvas', ...c)}
-export const caption = (...c) => { return t('caption', ...c)}
-export const cite = (...c) => { return t('cite', ...c)}
-export const code = (...c) => { return t('code', ...c)}
-export const col = (...c) => { return t('col', ...c)}
-export const colgroup = (...c) => { return t('colgroup', ...c)}
-export const command = (...c) => { return t('command', ...c)}
-export const datalist = (...c) => { return t('datalist', ...c)}
-export const dd = (...c) => { return t('dd', ...c)}
-export const del = (...c) => { return t('del', ...c)}
-export const details = (...c) => { return t('details', ...c)}
-export const dfn = (...c) => { return t('dfn', ...c)}
-export const div = (...c) => { return t('div', ...c)}
-export const dl = (...c) => { return t('dl', ...c)}
-export const dt = (...c) => { return t('dt', ...c)}
-export const em = (...c) => { return t('em', ...c)}
-export const embed = (...c) => { return t('embed', ...c)}
-export const fieldset = (...c) => { return t('fieldset', ...c)}
-export const figcaption = (...c) => { return t('figcaption', ...c)}
-export const figure = (...c) => { return t('figure', ...c)}
-export const footer = (...c) => { return t('footer', ...c)}
-export const form = (...c) => { return t('form', ...c)}
-export const h1 = (...c) => { return t('h1', ...c)}
-export const h2 = (...c) => { return t('h2', ...c)}
-export const h3 = (...c) => { return t('h3', ...c)}
-export const h4 = (...c) => { return t('h4', ...c)}
-export const h5 = (...c) => { return t('h5', ...c)}
-export const h6 = (...c) => { return t('h6', ...c)}
-export const head = (...c) => { return t('head', ...c)}
-export const header = (...c) => { return t('header', ...c)}
-export const hgroup = (...c) => { return t('hgroup', ...c)}
-export const hr = (...c) => { return t('hr', ...c)}
-export const html = (...c) => { return t('html', ...c)}
-export const i = (...c) => { return t('i', ...c)}
-export const iframe = (...c) => { return t('iframe', ...c)}
-export const img = (...c) => { return t('img', ...c)}
-export const input = (...c) => { return t('input', ...c)}
-export const ins = (...c) => { return t('ins', ...c)}
-export const kbd = (...c) => { return t('kbd', ...c)}
-export const keygen = (...c) => { return t('keygen', ...c)}
-export const label = (...c) => { return t('label', ...c)}
-export const legend = (...c) => { return t('legend', ...c)}
-export const li = (...c) => { return t('li', ...c)}
-export const link = (...c) => { return t('link', ...c)}
-export const main = (...c) => { return t('main', ...c)}
-export const map = (...c) => { return t('map', ...c)}
-export const mark = (...c) => { return t('mark', ...c)}
-export const menu = (...c) => { return t('menu', ...c)}
-export const meta = (...c) => { return t('meta', ...c)}
-export const meter = (...c) => { return t('meter', ...c)}
-export const nav = (...c) => { return t('nav', ...c)}
-export const noscript = (...c) => { return t('noscript', ...c)}
-export const object = (...c) => { return t('object', ...c)}
-export const ol = (...c) => { return t('ol', ...c)}
-export const optgroup = (...c) => { return t('optgroup', ...c)}
-export const option = (...c) => { return t('option', ...c)}
-export const output = (...c) => { return t('output', ...c)}
-export const p = (...c) => { return t('p', ...c)}
-export const param = (...c) => { return t('param', ...c)}
-export const pre = (...c) => { return t('pre', ...c)}
-export const progress = (...c) => { return t('progress', ...c)}
-export const q = (...c) => { return t('q', ...c)}
-export const rp = (...c) => { return t('rp', ...c)}
-export const rt = (...c) => { return t('rt', ...c)}
-export const ruby = (...c) => { return t('ruby', ...c)}
-export const s = (...c) => { return t('s', ...c)}
-export const samp = (...c) => { return t('samp', ...c)}
-export const script = (...c) => { return t('script', ...c)}
-export const section = (...c) => { return t('section', ...c)}
-export const select = (...c) => { return t('select', ...c)}
-export const small = (...c) => { return t('small', ...c)}
-export const source = (...c) => { return t('source', ...c)}
-export const span = (...c) => { return t('span', ...c)}
-export const strong = (...c) => { return t('strong', ...c)}
-export const style = (...c) => { return t('style', ...c)}
-export const sub = (...c) => { return t('sub', ...c)}
-export const summary = (...c) => { return t('summary', ...c)}
-export const sup = (...c) => { return t('sup', ...c)}
-export const table = (...c) => { return t('table', ...c)}
-export const tbody = (...c) => { return t('tbody', ...c)}
-export const td = (...c) => { return t('td', ...c)}
-export const textarea = (...c) => { return t('textarea', ...c)}
-export const tfoot = (...c) => { return t('tfoot', ...c)}
-export const th = (...c) => { return t('th', ...c)}
-export const thead = (...c) => { return t('thead', ...c)}
-export const time = (...c) => { return t('time', ...c)}
-export const title = (...c) => { return t('title', ...c)}
-export const tr = (...c) => { return t('tr', ...c)}
-export const track = (...c) => { return t('track', ...c)}
-export const underline = (...c) => { return t('underline', ...c)}
-export const ul = (...c) => { return t('ul', ...c)}
-export const _var = (...c) => { return t('var', ...c)}
-export const video = (...c) => { return t('video', ...c)}
-export const wbr = (...c) => { return t('wbr', ...c)}
+export const a = (...a) => { return t('a', ...a)}
+export const abbr = (...a) => { return t('abbr', ...a)}
+export const address = (...a) => { return t('address', ...a)}
+export const area = (...a) => { return t('area', ...a)}
+export const article = (...a) => { return t('article', ...a)}
+export const aside = (...a) => { return t('aside', ...a)}
+export const audio = (...a) => { return t('audio', ...a)}
+export const b = (...a) => { return t('b', ...a)}
+export const base = (...a) => { return t('base', ...a)}
+export const bdi = (...a) => { return t('bdi', ...a)}
+export const bdo = (...a) => { return t('bdo', ...a)}
+export const blockquote = (...a) => { return t('blockquote', ...a)}
+export const body = (...a) => { return t('body', ...a)}
+export const br = (...a) => { return t('br', ...a)}
+export const button = (...a) => { return t('button', ...a)}
+export const canvas = (...a) => { return t('canvas', ...a)}
+export const caption = (...a) => { return t('caption', ...a)}
+export const cite = (...a) => { return t('cite', ...a)}
+export const code = (...a) => { return t('code', ...a)}
+export const col = (...a) => { return t('col', ...a)}
+export const colgroup = (...a) => { return t('colgroup', ...a)}
+export const command = (...a) => { return t('command', ...a)}
+export const datalist = (...a) => { return t('datalist', ...a)}
+export const dd = (...a) => { return t('dd', ...a)}
+export const del = (...a) => { return t('del', ...a)}
+export const details = (...a) => { return t('details', ...a)}
+export const dfn = (...a) => { return t('dfn', ...a)}
+export const div = (...a) => { return t('div', ...a)}
+export const dl = (...a) => { return t('dl', ...a)}
+export const dt = (...a) => { return t('dt', ...a)}
+export const em = (...a) => { return t('em', ...a)}
+export const embed = (...a) => { return t('embed', ...a)}
+export const fieldset = (...a) => { return t('fieldset', ...a)}
+export const figcaption = (...a) => { return t('figcaption', ...a)}
+export const figure = (...a) => { return t('figure', ...a)}
+export const footer = (...a) => { return t('footer', ...a)}
+export const form = (...a) => { return t('form', ...a)}
+export const h1 = (...a) => { return t('h1', ...a)}
+export const h2 = (...a) => { return t('h2', ...a)}
+export const h3 = (...a) => { return t('h3', ...a)}
+export const h4 = (...a) => { return t('h4', ...a)}
+export const h5 = (...a) => { return t('h5', ...a)}
+export const h6 = (...a) => { return t('h6', ...a)}
+export const head = (...a) => { return t('head', ...a)}
+export const header = (...a) => { return t('header', ...a)}
+export const hgroup = (...a) => { return t('hgroup', ...a)}
+export const hr = (...a) => { return t('hr', ...a)}
+export const html = (...a) => { return t('html', ...a)}
+export const i = (...a) => { return t('i', ...a)}
+export const iframe = (...a) => { return t('iframe', ...a)}
+export const img = (...a) => { return t('img', ...a)}
+export const input = (...a) => { return t('input', ...a)}
+export const ins = (...a) => { return t('ins', ...a)}
+export const kbd = (...a) => { return t('kbd', ...a)}
+export const keygen = (...a) => { return t('keygen', ...a)}
+export const label = (...a) => { return t('label', ...a)}
+export const legend = (...a) => { return t('legend', ...a)}
+export const li = (...a) => { return t('li', ...a)}
+export const link = (...a) => { return t('link', ...a)}
+export const main = (...a) => { return t('main', ...a)}
+export const map = (...a) => { return t('map', ...a)}
+export const mark = (...a) => { return t('mark', ...a)}
+export const menu = (...a) => { return t('menu', ...a)}
+export const meta = (...a) => { return t('meta', ...a)}
+export const meter = (...a) => { return t('meter', ...a)}
+export const nav = (...a) => { return t('nav', ...a)}
+export const noscript = (...a) => { return t('noscript', ...a)}
+export const object = (...a) => { return t('object', ...a)}
+export const ol = (...a) => { return t('ol', ...a)}
+export const optgroup = (...a) => { return t('optgroup', ...a)}
+export const option = (...a) => { return t('option', ...a)}
+export const output = (...a) => { return t('output', ...a)}
+export const p = (...a) => { return t('p', ...a)}
+export const param = (...a) => { return t('param', ...a)}
+export const pre = (...a) => { return t('pre', ...a)}
+export const progress = (...a) => { return t('progress', ...a)}
+export const q = (...a) => { return t('q', ...a)}
+export const rp = (...a) => { return t('rp', ...a)}
+export const rt = (...a) => { return t('rt', ...a)}
+export const ruby = (...a) => { return t('ruby', ...a)}
+export const s = (...a) => { return t('s', ...a)}
+export const samp = (...a) => { return t('samp', ...a)}
+export const script = (...a) => { return t('script', ...a)}
+export const section = (...a) => { return t('section', ...a)}
+export const select = (...a) => { return t('select', ...a)}
+export const small = (...a) => { return t('small', ...a)}
+export const source = (...a) => { return t('source', ...a)}
+export const span = (...a) => { return t('span', ...a)}
+export const strong = (...a) => { return t('strong', ...a)}
+export const style = (...a) => { return t('style', ...a)}
+export const sub = (...a) => { return t('sub', ...a)}
+export const summary = (...a) => { return t('summary', ...a)}
+export const sup = (...a) => { return t('sup', ...a)}
+export const table = (...a) => { return t('table', ...a)}
+export const tbody = (...a) => { return t('tbody', ...a)}
+export const td = (...a) => { return t('td', ...a)}
+export const textarea = (...a) => { return t('textarea', ...a)}
+export const tfoot = (...a) => { return t('tfoot', ...a)}
+export const th = (...a) => { return t('th', ...a)}
+export const thead = (...a) => { return t('thead', ...a)}
+export const time = (...a) => { return t('time', ...a)}
+export const title = (...a) => { return t('title', ...a)}
+export const tr = (...a) => { return t('tr', ...a)}
+export const track = (...a) => { return t('track', ...a)}
+export const underline = (...a) => { return t('underline', ...a)}
+export const ul = (...a) => { return t('ul', ...a)}
+export const _var = (...a) => { return t('var', ...a)}
+export const video = (...a) => { return t('video', ...a)}
+export const wbr = (...a) => { return t('wbr', ...a)}
